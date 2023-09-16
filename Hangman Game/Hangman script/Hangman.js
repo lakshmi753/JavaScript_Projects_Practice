35934d12867df006a2526344407e1094bda5fddc
lakshmi753 <lakshmi753sahu@gmail.com>
2023-09-16 13:04:44 +0530

    Some functionalities were added in 'js and css files'
    Added functionality of displaying the correct 'Hangman image', displaying the 'game over modal' according to the game status and activating the 'play again' button

--- Hangman Game/Hangman script/Hangman.js
+++ Hangman Game/Hangman script/Hangman.js
@@ -1,7 +1,30 @@
 const keyboardDiv = document.querySelector(".keyboard-box");
 const guessingDiv = document.querySelector(".guess-item-box");
+const incorrectText = document.querySelector(".incorrect b");
+const hangmanImg = document.querySelector(".game-icon-pic");
+const gameOverModal = document.querySelector(".game-over-overlay");
+const playAgainBtn = document.querySelector(".play-again");
 
 let currentWord;
+let incorrectGuessCount;
+const maxguess = 6;
+let correctWord;
+
+const resetGame = function () {
+  // resseting all the variables ang UI elements...
+  incorrectGuessCount = 0;
+  correctWord = [];
+  hangmanImg.src = `images/hangman-${incorrectGuessCount}.svg`;
+  incorrectText.innerText = `${incorrectGuessCount}/${maxguess}`;
+  keyboardDiv
+    .querySelectorAll("button")
+    .forEach((btn) => (btn.disabled = false));
+  guessingDiv.innerHTML = currentWord
+    .split("")
+    .map(() => `<div class="guessed-letters"></div>`)
+    .join("");
+  gameOverModal.classList.remove("show");
+};
 
 const getRandomWord = function () {
   // getting a random word from the wordList..........
@@ -9,10 +32,26 @@ const getRandomWord = function () {
   currentWord = word;
   console.log(word, hint);
   document.querySelector(".hint b").innerText = hint;
-  guessingDiv.innerHTML = word
-    .split("")
-    .map(() => `<div class="guessed-letters"></div>`)
-    .join("");
+  resetGame();
+};
+
+const gameOver = (isvictory) => {
+  setTimeout(() => {
+    // setting the gameoever modal content acc. to the game status...
+    const modalText = isvictory
+      ? "You found the word :"
+      : "The correct word was :";
+    gameOverModal.querySelector(".game-over-gif").src = `images/${
+      isvictory ? "victory" : "lost"
+    }.gif`;
+    gameOverModal.querySelector(".game-over").innerText = `${
+      isvictory ? "Congrats!" : "Game over!"
+    }`;
+    gameOverModal.querySelector(
+      ".correct-word"
+    ).innerHTML = `${modalText} <b>${currentWord}</b>`;
+    gameOverModal.classList.add("show");
+  }, 300);
 };
 
 const initGame = function (button, clickedLetter) {
@@ -21,11 +60,23 @@ const initGame = function (button, clickedLetter) {
     // displaying all the guessed letters..........
     [...currentWord].forEach((currentLetter, index) => {
       if (currentLetter === clickedLetter) {
+        correctWord.push(currentLetter);
         guessingDiv.querySelectorAll("div")[index].innerText = currentLetter;
         guessingDiv.querySelectorAll("div")[index].classList.add("guessed");
       }
     });
+  } else {
+    // Updating the "incorrect counter" ang the "Hangman image"...
+    incorrectGuessCount++;
+    hangmanImg.src = `images/hangman-${incorrectGuessCount}.svg`;
   }
+
+  incorrectText.innerText = `${incorrectGuessCount}/${maxguess}`;
+  button.disabled = true;
+
+  // Calling gameOver function for the following conditions...
+  if (incorrectGuessCount === maxguess) return gameOver(false);
+  if (correctWord.length === currentWord.length) return gameOver(true);
 };
 
 // Creating keyboard buttons ............
@@ -42,3 +93,4 @@ for (let i = 97; i <= 122; i++) {
 }
 
 getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
