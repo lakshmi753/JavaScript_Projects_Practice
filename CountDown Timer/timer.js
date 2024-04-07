b4d92bda355823ac8c9b4b2e16a6415c3d138547
lakshmi753 <lakshmi753sahu@gmail.com>
2024-04-07 20:54:52 +0530

    Calculation part of the app is done, in file 'timer.js'.

--- CountDown Timer/timer.js
+++ CountDown Timer/timer.js
@@ -1,23 +1,43 @@
 const inputSetDateEl = document.getElementById("setDate");
+const setTimerBtn = document.getElementById("setTimer");
+const resetTimerBtn = document.getElementById("resetTimer");
+const timerDisplayBoxes = document.querySelectorAll(".count");
 
-inputSetDateEl.addEventListener("change", function (e) {
-  //console.log(e.target.value);
+function handleTimer(e) {
   const userDate = e.target.value;
   const endDate = new Date(userDate);
   console.log(endDate);
-  // console.log(Date.now(userDate));
+
   const presentDate = new Date();
   console.log(presentDate);
-  //console.log(Date.now(presentDate));
-  console.log(endDate - presentDate);
 
-  /* const usersDate = e.target.value;
-  const endingDate = Date.now(usersDate);
-  console.log(endingDate);
+  const timeDifference = endDate - presentDate;
+  console.log(timeDifference);
 
-  const presentDate = new Date();
-  const nowDate = Date.now(presentDate);
-  console.log(nowDate);
+  const timeDiffInSec = Math.floor(timeDifference / 1000);
+  console.log(timeDiffInSec);
 
-  console.log(endingDate - nowDate);*/
-});
+  // Number of days............
+  const days = Math.floor(timeDiffInSec / 86400); // 1 day = 86400 secs.
+  console.log(days);
+
+  // Number of hours..........
+  const remainingSecAfterDays = timeDiffInSec % 86400;
+  console.log(remainingSecAfterDays);
+  const hours = Math.floor(remainingSecAfterDays / 3600); // 1 hour = 3600 secs.
+  console.log(hours);
+
+  // Number of minutes..............
+  const remainingsecAfterHours = remainingSecAfterDays % 3600;
+  console.log(remainingsecAfterHours);
+  const minutes = Math.floor(remainingsecAfterHours / 60); // 1 minute = 60 secs.
+  console.log(minutes);
+
+  // Number of seconds................
+  const seconds = remainingsecAfterHours % 60;
+  console.log(seconds);
+}
+
+inputSetDateEl.addEventListener("change", handleTimer);
+
+//setInterval(handleTimer, 1000);
